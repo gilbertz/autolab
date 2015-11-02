e25fb504ecb07c0c42116c7e3e5002569a7fc683
gilbertz <gilbertzhaoguoqi@gmail.com>
2015-11-02 15:11:36 +0800

    change shell

--- doc/script/rapidUpdate.sh
+++ doc/script/rapidUpdate.sh
@@ -22,21 +22,21 @@ echo "set the permission 700 of .git"
 sudo chmod 700 .git
 
 echo "replace ip to 127.0.0.1"
-sudo sed -i "s/120.26.72.17/127.0.0.1/g" ./common/src/main/resources/application.properties
+sudo sed -i "s/120.26.72.17/127.0.0.1/g" ./api/src/main/resources/application.properties
 
 echo "hide sql"
-sudo sed -i "s/spring.jpa.show-sql/#spring.jpa.show-sql/g" ./common/src/main/resources/application.properties
+sudo sed -i "s/spring.jpa.show-sql/#spring.jpa.show-sql/g" ./api/src/main/resources/application.properties
 
 echo "change the log appender"
-sudo sed -i "s/<!--CONSOLE_COMMENT1-->/<!--CONSOLE_COMMENT1/g" ./common/src/main/resources/logback.xml
-sudo sed -i "s/<!--CONSOLE_COMMENT2-->/CONSOLE_COMMENT2-->/g" ./common/src/main/resources/logback.xml
-sudo sed -i "s/<!--FILE_COMMENT1/<!--FILE_COMMENT1-->/g" ./common/src/main/resources/logback.xml
-sudo sed -i "s/FILE_COMMENT2-->/<!--FILE_COMMENT2-->/g" ./common/src/main/resources/logback.xml
+sudo sed -i "s/<!--CONSOLE_COMMENT1-->/<!--CONSOLE_COMMENT1/g" ./api/src/main/resources/logback.xml
+sudo sed -i "s/<!--CONSOLE_COMMENT2-->/CONSOLE_COMMENT2-->/g" ./api/src/main/resources/logback.xml
+sudo sed -i "s/<!--FILE_COMMENT1/<!--FILE_COMMENT1-->/g" ./api/src/main/resources/logback.xml
+sudo sed -i "s/FILE_COMMENT2-->/<!--FILE_COMMENT2-->/g" ./api/src/main/resources/logback.xml
 
 
 echo "Remove the test module"
 sudo sed -i "s/testCompile/\/\/testCompile/g" ./build.gradle
-sudo sed -i "s/intTest/\/\/intTest/g" ./common/build.gradle
+sudo sed -i "s/intTest/\/\/intTest/g" ./api/build.gradle
 
 sudo chmod o+x gradlew
 
