337e62fcc3de2632626ab3ab27691bd86f1fba62
qinzhaokun <qinzhaokun@qq.com>
2015-11-09 16:29:56 +0800

    ff

--- api/src/main/java/com/autolab/api/service/CourseService.java
+++ api/src/main/java/com/autolab/api/service/CourseService.java
@@ -1,6 +1,17 @@
 package com.autolab.api.service;
 
+import com.autolab.api.exception.UtilException;
+import com.autolab.api.model.Course;
+import com.autolab.api.model.CourseTeacher;
+import com.autolab.api.model.User;
+import com.autolab.api.repository.CourseDao;
+import com.autolab.api.repository.CourseTeacherDao;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ABC on 2015/11/9 0009.
@@ -8,4 +19,63 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+
+    private static Logger logger = Logger.getLogger(CourseService.class);
+
+    @Autowired
+    private CourseTeacherDao courseTeacherDao;
+
+    @Autowired
+    private CourseDao courseDao;
+
+    public boolean checkAuth(User user, Course course){
+        CourseTeacher courseTeacher = courseTeacherDao.findByCourseAndTeacher(course, user);
+        List<CourseTeacher> courseTeachers = courseTeacherDao.findByCourse(course);
+
+        if(courseTeacher == null || courseTeachers == null){
+            throw new UtilException("there is no information in database");
+        }
+        if(courseTeachers.contains(courseTeacher)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param course
+     * @param teacher
+     */
+    public void createCourse(Course course, User teacher){
+        CourseTeacher courseTeacher = new CourseTeacher();
+        courseTeacher.setCourse(course);
+        courseTeacher.setTeacher(teacher);
+        courseTeacherDao.save(courseTeacher);
+        List<CourseTeacher> courseTeachers = new ArrayList<>();
+        courseTeachers.add(courseTeacher);
+        course.setCourseTeachers(courseTeachers);
+        courseDao.save(course);
+    }
+
+
+    /**
+     *
+     * @param course
+     * @param teacher
+     */
+    public void addTeacher(Course course, User teacher){
+        CourseTeacher courseTeacher = new CourseTeacher();
+        courseTeacher.setCourse(course);
+        courseTeacher.setTeacher(teacher);
+        courseTeacherDao.save(courseTeacher);
+        List<CourseTeacher> courseTeachers = course.getCourseTeachers();
+        if(courseTeachers == null){
+            throw new UtilException("database error");
+        }
+        courseTeachers.add(courseTeacher);
+        course.setCourseTeachers(courseTeachers);
+        courseDao.save(course);
+    }
 }
